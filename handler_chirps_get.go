3352c1d3be96b04cb8f99066f32838af91f59a52
ham-andres <asd.ray6@gmail.com>
2026-05-23 20:24:05 +0530

    added feature to get chirp by ID

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,6 +2,7 @@ package main
 
 import (
 		"net/http"
+		"github.com/google/uuid"
 		
 
 )
@@ -28,4 +29,29 @@ func (cfg *apiConfig) handlerGetChirps(resw http.ResponseWriter, req *http.Reque
 		respondWithJSON(resw, http.StatusOK, chirps)
 
 	
+}
+
+func (cfg *apiConfig) handlerGetChirpByID( resw http.ResponseWriter, req *http.Request) {
+		chirpId, err := uuid.Parse(req.PathValue("chirpID"))
+		if err != nil {
+			respondWithError(resw, http.StatusBadRequest, "Invalid ID", err)
+			return
+		}
+
+		receivedChirp, err := cfg.db.GetChirpByID(req.Context(), chirpId)
+		if err != nil {
+			respondWithError(resw, http.StatusNotFound," Couldn't found chirp of given ID", err)
+			return 
+		}
+
+		chirp := respondChirp{
+			ID:		receivedChirp.ID,
+			CreatedAt: 		receivedChirp.CreatedAt,
+			UpdatedAt:		receivedChirp.UpdatedAt,
+			Body:					receivedChirp.Body,
+			UserID:				receivedChirp.UserID,
+		}
+
+		respondWithJSON(resw, http.StatusOK, chirp)
+
 }
--- sql/queries/retrieve_chirps.sql
+++ sql/queries/retrieve_chirps.sql
@@ -1,3 +1,6 @@
 -- name: RetrieveChirps :many 
 SELECT * FROM chirps ORDER BY created_at ASC;
 --
+-- name: GetChirpByID :one 
+SELECT * FROM chirps WHERE id = $1;
+--
